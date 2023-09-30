5d8451beb5ea54acd9755bc8c05492cecb3342f0
moura <muradhani>
2023-09-30 17:07:10 +0300

    data for single meal displaying in xml using observing not data binding

--- app/src/main/java/com/example/foodapp/state/State.kt
+++ app/src/main/java/com/example/foodapp/state/State.kt
@@ -4,5 +4,10 @@ sealed class State<out T>{
     data class Error(val meassage:String):State<Nothing>()
     data class Success<T>(val data:T):State<T>()
     object Loading:State<Nothing>()
-    fun toData() = if (this is State.Success) data else null
+    fun toData():T? {
+        if (this is Success){
+            return data
+        }
+        return null
+    }
 }
